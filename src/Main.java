a5166298754a0c93353707b86167715cf4a8942e
patwyzlic <patrickwyzlic@gmail.com>
2022-09-24 13:34:11 -0400

    Declaring int, double, char, and boolean

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int studentAge = 18;
+        double studentGPA = 3.12;
+        char studentFirstInitial = 'P';
+        char studentLastInitial = 'W';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
     }
 }
