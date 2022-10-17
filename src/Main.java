c807e776256c667d551e6216edcf2eb08b79408c
patwyzlic <patrickwyzlic@gmail.com>
2022-10-17 16:10:02 -0400

    Tea Time Function

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void announceDeveloperTeaTime(){
+        System.out.println("Waiting for dev tea time...");
+        System.out.println("Type word and press enter");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("Time for tea!");
+    }
 
+    public static void main(String[] args) {
+        announceDeveloperTeaTime();
     }
 
 }
